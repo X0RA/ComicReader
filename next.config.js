514459d6652f75ff9ab4f17ca229927bfe09c833
Tenny <annand.rodney@gmail.com>
2025-03-19 17:20:43 +1100

    added meta tags but no images yet, testing the service worker

--- next.config.js
+++ next.config.js
@@ -8,8 +8,9 @@ const nextConfig = {
 const pwaConfig = withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development'
+  // skipWaiting: true,
+  // scope: '/app',
+  disable: process.env.NODE_ENV === 'development'
 })(nextConfig);
 
 module.exports = pwaConfig; 
